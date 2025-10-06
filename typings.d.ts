5a3ceb17c0d1fa7e1bf01439ce361c9650836d82
Eric Dean Kauffman <eric@moonrise.net>
2025-10-06 13:07:08 -0500

    Begining Update

---
+++ typings.d.ts
@@ -0,0 +1,11 @@
+declare module '@ckeditor/ckeditor5-dev-utils' {
+    const ClassicEditorBuild: any;
+
+    export = ClassicEditorBuild;
+}
+
+declare module '@ckeditor/ckeditor5-dev-translations' {
+    const ClassicEditorBuild: any;
+
+    export = ClassicEditorBuild;
+}
